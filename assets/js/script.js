a6144e947e804cd3cbf459acf0052aa38de0fd05
poofy <skyang014@gmail.com>
2022-04-30 20:20:16 -0700

    pseudocode finished

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// global variables
+    //score
+    //create questions array
+    //create right answers array
+    //create wrong answers array
+
+// Timer
+
+
+// when clicked, run this code
+    // start timer
+
+    // game over function if  i > questions length OR timer <= 0 
+        //display 'done'
+        //display score
+        //enter initials
+        //save data locally
+        //display 'high scores'
+
+    // if i < quetions.length
+        //dynamically create question, h2 in div
+        //dynamically create correct answer
+        //dynamically create wrong answers
+
+    //if correct answer clicked
+        //display correct
+        // score = score + 1
+        //go to next question, display i+1
+
+    //if wrong answer clicked
+        //display wrong
+        //timer = timer - 5000ms
+        //go to next question, display i+1
+
